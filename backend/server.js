3daaec70ff82a9b882c0537770d847a1e8236298
PalAnghan <palanghan750@gmail.com>
2026-02-02 18:22:32 +0530

    Meaningful message

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());        // 👈 IMPORTANT
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend running");
+  res.json({ message: "Backend connected successfully" });
 });
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log("Backend running on port 5000");
 });
