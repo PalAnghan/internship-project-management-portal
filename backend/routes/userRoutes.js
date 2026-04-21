8e6a8a7e0fe78271c4fd27d182f2998d7ddc8449
PalAnghan <palanghan750@gmail.com>
2026-04-21 14:10:17 +0530

    added get all users route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -154,4 +154,21 @@ router.post(
  }
 );
 
+// GET ALL USERS (for testing)
+router.get("/", async (req, res) => {
+ try {
+
+  const users = await User.find();
+
+  res.json(users);
+
+ } catch (err) {
+
+  res.status(500).json({
+   message: "Error fetching users"
+  });
+
+ }
+});
+
 module.exports = router;
